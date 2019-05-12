9d5b6b62ccaa111891e20c20c3dfaaaa45ea5dde
Mr. Nopasinth  Vithayapalert <wiw@DN0a1e2b8d.SUNet>
2019-05-11 23:52:05 -0700

    lstm code added

--- LSTM_model.py
+++ LSTM_model.py
@@ -1,9 +1,20 @@
 from alpha_model import AlphaModel
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import InputLayer, Input
+from keras.layers import Dense, LSTM
 
 class LSTMModel(AlphaModel):
     def __init__(self):
         self.model = 0
+        model = Sequential()
+        model.add(LSTM(units=30, return_sequences= True, input_shape=(X.shape[1],2)))
+        model.add(LSTM(units=30, return_sequences=True))
+        model.add(LSTM(units=30))
+        model.add(Dense(units=1))
 
     def train(self, X, Y):
         assert len(X)==len(Y)
