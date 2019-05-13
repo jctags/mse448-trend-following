991bd4a2c3911e8760d4847a5c36e68f10cd0bba
Methin <methin@DN0a1fe634.SUNet>
2019-05-12 19:09:47 -0700

    updated baseline strategy

--- baseline_strat.py
+++ baseline_strat.py
@@ -12,23 +12,24 @@ def buyCrossStrat(cross_array, price_array):
     units_over_time = []
     i = 0;
     cond = 1;
-
     for row in df.iterrows():
-        if df["EMA100Cross"][i] == 1 and cond == 1:
-            units =  money/df["Settle_Price"][i]
-            lastprice = df["Settle_Price"][i]
+        if cross_array[i] == 1 and cond == 1:
+            units =  money/price_array[i]
+            lastprice = price_array[i]
             cond = 2
         if cond == 2:
-            if df["EMA100Cross"][i] == -1 or df["Settle_Price"][i] >= 1.10 * lastprice or df["Settle_Price"][i] <= 0.95 * lastprice:
-                money = (units) * df["Settle_Price"][i]
+            if cross_array[i] == -1 or price_array[i] >= 1.10 * lastprice or price_array[i] <= 0.95 * lastprice:
+                money = (units) * price_array[i]
                 units =  0
                 cond = 1
         money_over_time.append(money)
         units_over_time.append(units)
-    #    lastprice_over_time.append(lastprice)
         i = i + 1;
+    return money, money_over_time, units_over_time
 
-    return money
+def main():
+    money, money_array, unit_arry = buyCrossStrat(df["EMA100Cross"],df["Settle_Price"])
+    print(money)
 
 if __name__ == "__main__":
     main()
