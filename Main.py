3e68a3691885bfeeed769be6729732acbf6d069d
Mr. Nopasinth  Vithayapalert <wiw@DN0a1e2b8d.SUNet>
2019-05-28 14:41:52 -0700

    Updated Main.py

--- Main.py
+++ Main.py
@@ -59,18 +59,44 @@ def main():
         'SMA20',
         'SMA5',
         'SMA50',
-        # 'Settle_Price'
     ]
-    label = 'Settle_Price'
-    features_directory = 'data'
-
+    label = 'Daily_Return'
+    data_directory = 'data'
     valid_start = 2017
     test_start = 2017
     data_end = 2019
+    predicted_returns = dict()
+    actual_returns = dict()
+    train_returns = dict()
 
     scale = MinMaxScaler(feature_range=(0, 1))
-    for i, filename in enumerate(glob.glob("data/*.csv")):
-        df = get_dataframe(filename)
+
+    print("Loading Dataframes")
+    dfs = []
+    for i, filename in enumerate(os.listdir(data_directory)):
+        if '.csv' not in filename:
+            continue
+        df = get_dataframe(data_directory + '/' + filename)
+        dfs.append(df)
+
+    print("Standardizing Date")
+    minimal_dates = None
+    for df in dfs:
+        dates = set(df['Date'].map(lambda x: x.timestamp()))
+        if minimal_dates is None:
+            minimal_dates = dates
+        else:
+            minimal_dates = minimal_dates & dates
+
+    new_dfs = []
+
+    for df in dfs:
+        new_df = df[df['Date'].map(lambda x: x.timestamp() in minimal_dates)]
+        new_dfs.append(new_df)
+
+    dfs = new_dfs
+
+    for i, df in enumerate(dfs):
         data, date = get_data(df, valid_start, test_start, data_end, features, label)
         data['Xtrain'] = scale.fit_transform(data['Xtrain'])
         data['Ytrain'] = scale.fit_transform(data['Ytrain'].reshape(-1,1))
@@ -81,13 +107,20 @@ def main():
         print(trainX.shape, trainY.shape, testX.shape, testY.shape)
         model = LSTMModel()
         model.train(trainX, trainY, look_back)
-        predictedY = model.predict(testX)
-        predictedY = np.array(scale.inverse_transform(predictedY).ravel()).astype(str)
+        y_pred = model.predict(testX)
+        y_pred = scale.inverse_transform(y_pred).ravel()
+        predicted_returns[str(i)] = y_pred
+        actual_returns[str(i)] = data['Ytest']
+        train_returns[str(i)] = data['Ytrain']
         date['test'] = np.array(date['test'][look_back + 1:]).astype(str)
-        pred = np.vstack((predictedY, date['test']))
-        pred = np.transpose(pred)
-        pred_df = pd.DataFrame(pred)
-        pred_df.to_csv('LSTM_output/predicted_price_' + filename[5:])
+        # pred = np.vstack((predictedY, date['test']))
+        # pred = np.transpose(pred)
+        # pred_df = pd.DataFrame(pred)
+        # pred_df.to_csv('LSTM_output/predicted_price_' + filename[5:])
+
+    pred_df = pd.DataFrame(predicted_returns)
+    pred_df.to_csv('LSTM_output/predicted_price')
+    actual_df = pd.DataFrame(actual_returns)
 
 if __name__ == "__main__":
     main()
